c1714f3967fd9115b4c46e5eb6afc21faf399d92
gyuion <168659826+sallkyy@users.noreply.github.com>
2024-11-16 22:50:37 +0300

    Update dashboard.php
    try1

--- dashboard.php
+++ dashboard.php
@@ -75,7 +75,7 @@ $user = $_SESSION['user'] ?? null; // Проверка авторизации п
   </div>
 </nav>
 <div class="form-container">
-  <h1>Приветствуем на предприятии Завод</h1>
+  <h1>Приветствуем на предприятии завода</h1>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
